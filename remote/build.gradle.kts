562898d9e10f0217d8e6d9faee56ab5048cf448f
ShashankKasera <ShashankKasera98@gmail.com>
2021-12-04 18:30:21 +0530

    remote init

---
+++ remote/build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+plugins {
+    id(Plugins.LIBRARY)
+    kotlin(Plugins.KOTLIN_ANDROID)
+    id(Plugins.DAGGER_HILT_PLUGIN)
+    kotlin(Plugins.KOTLIN_KAPT)
+}
+
+android {
+    compileSdk = AppConfig.compileSdk
+    buildToolsVersion = AppConfig.buildToolsVersion
+
+    defaultConfig {
+        minSdk =  AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        testInstrumentationRunner = AppConfig.androidTestInstrumentation
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = Versions.JVM_TARGET
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+}
+
+dependencies {
+
+    testImplementation(AppDependencies.testLibraries)
+    androidTestImplementation(AppDependencies.androidTestLibraries)
+
+    api(AppDependencies.remoteLibraries)
+    daggerHilt()
+    api(project(mapOf("path" to AppDependencies.DATA)))
+}
